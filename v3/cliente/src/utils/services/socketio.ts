f07bb20a45fc2a65cff3150eff78cac757c3edcc
André Cabral <63832522+AndreLucas2103@users.noreply.github.com>
2022-09-27 22:50:20 +0000

    1

--- v3/cliente/src/utils/services/socketio.ts
+++ v3/cliente/src/utils/services/socketio.ts
@@ -1,3 +1,3 @@
 import socketIOClient from "socket.io-client";
 
-export const socket = socketIOClient("http://localhost:3030");
+export const socket = socketIOClient(import.meta.env.VITE_APP_URL_SOCKETIO);
--- v3/usuario/src/utils/services/socketio.ts
+++ v3/usuario/src/utils/services/socketio.ts
@@ -1,6 +1,6 @@
 import socketIOClient from "socket.io-client";
 
-export const socket = socketIOClient("http://localhost:3030", {
+export const socket = socketIOClient(import.meta.env.VITE_APP_URL_SOCKETIO, {
     reconnection: true,
     auth: {
         token: ""
